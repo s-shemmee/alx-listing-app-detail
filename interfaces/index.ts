c406f788ca147d657dc2c4a97a5a163a5112ca45
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2025-07-20 15:00:58 +0100

    Add sample property listings and define PropertyProps interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,3 +1,22 @@
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
+
 export interface CardProps {
   title: string;
   imageSrc: string;
@@ -7,5 +26,6 @@ export interface CardProps {
 export interface ButtonProps {
   text: string;
   onClick?: () => void;
-  variant?: "primary" | "secondary";
+  variant?: 'primary' | 'secondary';
+  className?: string;
 }
